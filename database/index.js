45da688f3e40e67e98aa6f54208d015d5467c7de
jGilarana <jorgereboot@gmail.com>
2023-11-16 10:05:54 +0000

    connection done

---
+++ database/index.js
@@ -0,0 +1,32 @@
+const { Sequelize } = require('sequelize')
+require('dotenv').config()
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DIALECT,
+  port: process.env.DB_PORT,
+  logging: false
+}); //DB CREATION
+
+
+
+async function checkConn() { //DB CONN checking function
+  try {
+    await sequelize.authenticate()
+    console.log('Nice, you are connected!')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function syncModels() {
+  try {
+    await sequelize.sync({alter : true})
+    console.log("sync models succesful")
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+module.exports = { checkConn, syncModels, sequelize }
--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const j = 'hola mundo'
+const { checkConn, syncModels } = require("./database")
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
-console.log(j)
+const app = express()
+
+
+
+
+
+async function connectDB() {
+
+    await checkConn()
+    /* setRelations() */
+    await syncModels()
+
+}
+
+
+function launchServer() {
+
+    app.use(cors()) //permite piticiones de todas las direcciones
+        .use(morgan('dev')) //nos muestra las peticiones en consola
+        .use(express.json()) // permite leer formato json en el body
+        /* .use('/api', require('./api/routes/index')) */
+
+        .listen(process.env.EXPRESS_PORT, () => console.log('Server listening on port 3000')) //server listening requests
+}
+
+async function startAPI() {
+
+    launchServer()
+    connectDB()
+}
+
+
+startAPI()
