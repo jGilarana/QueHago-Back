30ab7b8b8610d090af9c43ef2fd332e9e5647ad5
jGilarana <jorgereboot@gmail.com>
2023-11-18 19:56:12 +0000

    login done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -25,6 +25,40 @@ async function signup(req, res) {
     }
 }
 
+
+async function login(req, res) {
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+
+        if (!user) return res.status(404).send('Error: Email  incorrect')
+
+        const comparePass = bcrypt.compareSync(req.body.password, user.password)
+
+        if (comparePass) {
+            const payload = {
+                email: user.email
+            }
+            const token = jwt.sign(payload, process.env.SECRET, {
+                expiresIn: '1h'
+            })
+            return res.status(200).json({
+                token
+            })
+        } else {
+            return res.status(404).json('Error: Email or Password incorrect')
+        }
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+
+
 module.exports = {
-  signup
+  signup,
+  login
 }
