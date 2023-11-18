e51165fdd3c4528fc3e6b794138124f38c955dd1
jGilarana <jorgereboot@gmail.com>
2023-11-18 23:35:48 +0000

    authorization done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,62 +3,59 @@ const User = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 
 async function signup(req, res) {
-    const saltRounds = bcrypt.genSaltSync(parseInt(process.env.SALTROUNDS))
-    const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds) // Hash the original password with the number we have provided.
-    req.body.password = hashedPassword // update the body's password with the hashed password
+  const saltRounds = bcrypt.genSaltSync(parseInt(process.env.SALTROUNDS))
+  const hashedPassword = bcrypt.hashSync(req.body.password, saltRounds) // Hash the original password with the number we have provided.
+  req.body.password = hashedPassword // update the body's password with the hashed password
 
-    try {
-        const user = await User.create(req.body) // create the user with the hashed password
-      
-        const payload = {
-            email: user.email
-        }
-        const token = jwt.sign(payload, process.env.SECRET, {
-            expiresIn: '1h'
-        })
-        return res.status(200).json({
-            token
-        })
-    } catch (error) {
-        console.log(error.message) // Log the error message for debugging
-        return res.status(500).send(error.message)
+  try {
+    const user = await User.create(req.body) // create the user with the hashed password
+
+    const payload = {
+      email: user.email,
     }
+    const token = jwt.sign(payload, process.env.SECRET, {
+      expiresIn: "1h",
+    })
+    return res.status(200).json({
+      token,
+    })
+  } catch (error) {
+    console.log(error.message) // Log the error message for debugging
+    return res.status(500).send(error.message)
+  }
 }
-
 
 async function login(req, res) {
-    try {
-        const user = await User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    })
 
-        if (!user) return res.status(404).send('Error: Email  incorrect')
+    if (!user) return res.status(404).send("Error: Email  incorrect")
 
-        const comparePass = bcrypt.compareSync(req.body.password, user.password)
+    const comparePass = bcrypt.compareSync(req.body.password, user.password)
 
-        if (comparePass) {
-            const payload = {
-                email: user.email
-            }
-            const token = jwt.sign(payload, process.env.SECRET, {
-                expiresIn: '1h'
-            })
-            return res.status(200).json({
-                token
-            })
-        } else {
-            return res.status(404).json('Error: Email or Password incorrect')
-        }
-    } catch (error) {
-        return res.status(500).send(error.message)
+    if (comparePass) {
+      const payload = {
+        email: user.email,
+      }
+      const token = jwt.sign(payload, process.env.SECRET, {
+        expiresIn: "1h",
+      })
+      return res.status(200).json({
+        token,
+      })
+    } else {
+      return res.status(404).json("Error: Email or Password incorrect")
     }
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
-
-
 
 module.exports = {
   signup,
-  login
+  login,
 }
--- api/routers/favorite.router.js
+++ api/routers/favorite.router.js
@@ -1,7 +1,8 @@
 const {getAllFavorites, getOneFavorite, createFavorite, updateFavorite, deleteFavorite} = require('../controllers/favorite.controller')
+const { checkAuth } = require('../middleware')
 const router = require('express').Router()
 
-router.get('/', getAllFavorites)
+router.get('/', checkAuth, getAllFavorites)
 router.get('/:id', getOneFavorite)
 router.post('/', createFavorite)
 router.put('/:id', updateFavorite)
